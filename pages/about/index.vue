28846df1a26fe718ee225a8c94130db5b32ac52a
Dany Walraven <dwalraven21@gmail.com>
2019-09-30 22:03:52 -0400

    set up pages structure and added some basic css for single blog post

---
+++ pages/about/index.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="about-page">
+	<h1>This is my fantastic about page!</h1>
+  	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+.about-page {
+	text-align: center;
+	padding: 30px;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="posts-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+.posts-page {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
