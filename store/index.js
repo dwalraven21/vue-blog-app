2aa36c29abe542be03da98f64d6401d857c64d64
Dany Walraven <dwalraven21@gmail.com>
2019-10-11 16:45:20 -0400

    utilized cookies on server and local storage on client to persist login state or token

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from 'vuex'
+import Cookie from 'js-cookie'
 
 const createStore = () => {
 	return new Vuex.Store({
@@ -77,6 +78,8 @@ const createStore = () => {
 					vuexContext.commit('setToken', result.idToken)
 					vuexContext.setItem('token', result.idToken)
 					vuexContext.setItem('tokenExpiration', new Date().getTime() + result.expiresIn * 1000)
+					Cookie.set('jwt', result.idToken)
+					Cookie.set('expirationDate', new Date().getTime() + result.expiresIn * 1000)
 					vuexContext.dispatch('setLogoutTimer', result.expiresIn * 1000)
 				})
 		  		.catch((err) => console.log(err))
@@ -86,13 +89,33 @@ const createStore = () => {
 					vuexContext.commit('clearToken')
 				}, duration)
 			},
-			initAuth(vuexContext) {
-				const token = localStorage.getItem('token')
-				const expirationDate = localStorage.getItem('tokenExpiration')
+			initAuth(vuexContext, req) {
+				let token
+				let expirationDate
 
-				if ( new Date() > expirationDate || !token ) {
-					return
+				if (req) {
+					if (!req.headers.cookie) {
+						return
+					}
+					const jwtCookie = req.headers.cookie.split(';').find(c => c.trim().startsWith('jwt='))
+					if (!jwtCookie) {
+						return
+					}
+					token = jwtCookie.split('=')[1]
+					expirationDate = req.headers.cookie
+					.split(';')
+					.find(c => c.trim().startsWith('expirationDate='))
+					.split('=')[1]
+				} else {
+					token = localStorage.getItem('token')
+					expirationDate = localStorage.getItem('tokenExpiration')
+
+					if ( new Date().getTime() > +expirationDate || !token ) {
+						return
+					}
 				}
+
+				vuexContext.dispatch('setLogoutTimer', +expirationDate - new Date().getTime())
 				vuexContext.commit('setToken', token)
 			}
 		},
